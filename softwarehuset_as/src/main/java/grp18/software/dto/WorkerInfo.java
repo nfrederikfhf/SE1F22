2682e0318143d1d474a04e62b12b371549b5005a
s204176 <s204176@student.dtu.dk>
2022-03-21 16:43:56 +0100

    Worker almostdone

--- softwarehuset_as/src/main/java/grp18/software/dto/WorkerInfo.java
+++ softwarehuset_as/src/main/java/grp18/software/dto/WorkerInfo.java
@@ -1,4 +1,21 @@
 package grp18.software.dto;
 
+import java.util.ArrayList;
+
 public class WorkerInfo {
+
+    private String intials;
+    private int hoursWorked;
+    private List<Activities> activityRepository = new ArrayList<>();
+    private List<Event> eventRepository = new ArrayList<>();
+    private List<Managementlist> managementlistRepository = new ArrayList<>();
+
+
+    public WorkerInfo()
+
+    public void deleteEvent(){
+
+    }
+
+
 }
