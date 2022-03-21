80c865f05867011c1070dabda85135194141df00
s194050 <s194050@student.dtu.dk>
2022-03-21 16:41:48 +0100

    Further work on setup file

--- softwarehuset_as/src/main/java/grp18/software/app/System.java
+++ softwarehuset_as/src/main/java/grp18/software/app/System.java
@@ -1,4 +1,13 @@
 package grp18.software.app;
 
+import grp18.software.domain.Project;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class System {
+    private List<Project> projectRepository = new ArrayList<>();
+
+    
+
 }
