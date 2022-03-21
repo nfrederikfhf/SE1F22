92bf1bb9f6dee76f0b2f08a1ec4be168fbbdd19e
nfrederikfhf <s194053@student.dtu.dk>
2022-03-21 15:25:13 +0100

    save jacob

--- softwarehuset_as/src/main/java/grp18/software/app/Project.java
+++ softwarehuset_as/src/main/java/grp18/software/app/Project.java
@@ -1,9 +1,15 @@
 package grp18.software.app;
 
+import grp18.software.domain.Worker;
+import java.util.Calendar;
+
 public class Project{
     private String name;
     private int ID;
-    
+    private Worker projectManager;
+    private Calendar startDate;
+    private Calendar endDate;
+
 
 
 }
