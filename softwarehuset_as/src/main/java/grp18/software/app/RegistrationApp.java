12789fcc4262a6192769248652ff3fafc2428f1a
nfrederikfhf <s194053@student.dtu.dk>
2022-03-27 21:16:15 +0200

    Date server set up and system app made

---
+++ softwarehuset_as/src/main/java/grp18/software/app/RegistrationApp.java
@@ -0,0 +1,18 @@
+package grp18.software.app;
+
+import grp18.software.domain.Project;
+import grp18.software.domain.Worker;
+
+import java.util.*;
+
+public class RegistrationApp extends Observable{
+
+    private List<Worker> workers = new ArrayList<>();
+    private List<Project> projects = new ArrayList<>();
+    private DateServer dateServer = new DateServer();
+
+
+
+
+
+}
