40762f796feb0517385e1938126b9a7cf02c2a41
JHellum <LeoTheo1234>
2022-04-03 16:27:52 +0200

    change to string int

--- softwarehuset_as/src/test/java/grp18/software/acceptanceTests/ProjectSteps.java
+++ softwarehuset_as/src/test/java/grp18/software/acceptanceTests/ProjectSteps.java
@@ -4,39 +4,43 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
 public class ProjectSteps {
 
-    @Given("a project with number {string} and name {string} exists")
+    @Given("a project with number {int} and name {string} exists")
     public void a_project_with_number_and_name_exists(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("the project manager of project {string} adds an activity with name {string} to project {string}")
+    @When("the project manager of project {int} adds an activity with name {string} to project {int}")
     public void the_project_manager_of_project_adds_an_activity_with_name_to_project(String string, String string2, String string3) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the activity with name {string} is contained in project {string}'s list of activities")
+    @Then("the activity with name {string} is contained in project {int}'s list of activities")
     public void the_activity_with_name_is_contained_in_project_s_list_of_activities(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Given("the worker {string} has been assigned project manager of project {string}")
+    @Given("the worker {string} has been assigned project manager of project {int}")
     public void the_worker_has_been_assigned_project_manager_of_project(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @Given("the activity with name {string} is contained in project {string}'s list of activities")
+    @Given("the activity with name {string} is contained in project {int}'s list of activities")
     public void the_activity_with_name_is_contained_in_project_s_list_of_activities(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("the project manager of project {string} assigns the worker {string} to {string} of project {string}")
+    @When("the project manager of project {int} assigns the worker {string} to {string} of project {int}")
     public void the_project_manager_of_project_assigns_the_worker_to_of_project(String string, String string2, String string3, String string4) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
@@ -48,19 +52,16 @@ public class ProjectSteps {
         throw new io.cucumber.java.PendingException();
     }
 
-    @Then("the project {string} does not have the worker {string} in its worker list")
+    @Then("the project {int} does not have the worker {string} in its worker list")
     public void the_project_does_not_have_the_worker_in_its_worker_list(String string, String string2) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-    @When("the project manager of project {string} assigns the worker {string} to project {string}")
+    @When("the project manager of project {int} assigns the worker {string} to project {int}")
     public void the_project_manager_of_project_assigns_the_worker_to_project(String string, String string2, String string3) {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
-
-
-
 
 }
