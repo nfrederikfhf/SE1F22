03a67e79f0d3b5057f0ac2921d412d58123c51ce
s204176 <s204176@student.dtu.dk>
2022-04-04 16:33:46 +0200

    Update step

--- softwarehuset_as/src/main/java/grp18/software/domain/Activity.java
+++ softwarehuset_as/src/main/java/grp18/software/domain/Activity.java
@@ -1,12 +1,26 @@
 package grp18.software.domain;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 public class Activity {
     private String activityName;
     private Calendar startDate;
     private Calendar endDate;
-    private int timeBudget;
     private Worker worker;
+
+    private String initials;
+    private List<Activity> activities = new ArrayList<>();
+    private List<Event> events = new ArrayList<>();
+    private List<Project> managementList = new ArrayList<>();
+
+    public Activity(String initials){
+        this.initials = initials;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
 
 }
