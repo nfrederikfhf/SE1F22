b91ad74c9deaf21c4cbdec9203f0010b6366a276
s204176 <s204176@student.dtu.dk>
2022-04-04 15:37:06 +0200

    Update ActivityInfo.java

--- softwarehuset_as/src/main/java/grp18/software/dto/ActivityInfo.java
+++ softwarehuset_as/src/main/java/grp18/software/dto/ActivityInfo.java
@@ -1,4 +1,32 @@
 package grp18.software.dto;
 
+import java.util.Calendar;
+
 public class ActivityInfo {
+
+    private String activityName;
+    private Calendar startDate;
+    private Calendar endDate;
+    private int timeBudget;
+
+    public ActivityInfo(String activityName, Calendar startDate, Calendar endDate, int timeBudget){
+        this.activityName = activityName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+
+    }
+
+    private void addWorker(Worker worker){
+
+    }
+
+    private void deleteWorker(Worker worker){
+
+    }
+
+    private String activityStatus(){
+
+    }
+
+
 }
